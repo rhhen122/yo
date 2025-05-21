090d19ce58c5d3fe4ddd3025c9e790f10497510a
Roky Henderson - rhhen <rokyhenderson222@gmail.com>
2025-05-22 11:52:08 +1200

    Added more config options

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,24 @@
 fn main() {
-    let name = "bro or bruz (:";
-    println!("Yo, Hope your doing well {}", name);
+    // Config
+    let name = "bro or bruz (:"; // Change what it calls you.
+    let credit = false; // Adds Credit from me (:
+
+
+
+    let creditmsg = " - Made by Roky Henderson";
+
+    if credit == true {
+        yeet(true, name, creditmsg);
+    } else {
+        yeet(false, name, creditmsg);
+    }
+}
+
+fn yeet(credit: bool, name: &'static str, creditmsg: &'static str) {
+    if credit == true {
+        println!("Yo, Hope your doing well {} {}", name, creditmsg);
+    }
+    if credit == false {
+        println!("Yo, Hope your doing well {}", name);
+    }
 }

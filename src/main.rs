745fbc87b8565bbb7f61397910d2e404bfb89475
Roky Henderson - rhhen <rokyhenderson222@gmail.com>
2025-05-22 12:07:21 +1200

    Added Color

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,43 @@
+use colored::Colorize;
 fn main() {
     // Config
     let name = "bro or bruz (:"; // Change what it calls you.
     let credit = false; // Adds Credit from me (:
+    let color = true; // Makes the 'Yo, ' Yellow.
 
 
 
     let creditmsg = " - Made by Roky Henderson";
 
     if credit == true {
-        yeet(true, name, creditmsg);
+        if color == true {
+            yeet(true, true, name, creditmsg);
+        } else {
+            println!("Yo, Hope your doing well {}", name);
+        }
     } else {
-        yeet(false, name, creditmsg);
+        if color == true {
+            yeet(true, false, name, creditmsg);
+        } else {
+            println!("Yo, Hope your doing well {}", name);
+        }
     }
 }
 
-fn yeet(credit: bool, name: &'static str, creditmsg: &'static str) {
+fn yeet(color: bool, credit: bool, name: &'static str, creditmsg: &'static str) {
     if credit == true {
-        println!("Yo, Hope your doing well {} {}", name, creditmsg);
+        if color == true {
+            println!("{}Hope your doing well {} {}", "Yo, ".yellow(), name, creditmsg);
+        } else {
+            println!("Yo, Hope your doing well {}", name);
+        }
     }
     if credit == false {
-        println!("Yo, Hope your doing well {}", name);
+        if color == true {
+            println!("{}Hope your doing well {}", "Yo, ".yellow(), name);
+        } else {
+            println!("Yo, Hope your doing well {}", name);
+        }
+        
     }
 }
